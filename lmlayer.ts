162597f00cf4c52ca1d955bbdf0979e2b87fab97
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-09 14:14:24 -0700

    Commmiting what there is of my failed refactor.

--- lmlayer.ts
+++ lmlayer.ts
@@ -1,3 +1,5 @@
+import { type } from "os";
+
 /**
  * Prototype LMLayer.
  *
@@ -42,167 +44,188 @@ interface InternalSuggestion {
 }
 
 type PostMessage = typeof DedicatedWorkerGlobalScope.prototype.postMessage;
+type ModelFactory = (c: RequestedConfiguration) => Model;
 
 /**
  * Handles the protocol for communicating with the keyboard.
  *
- * A few methods can be easily mocked for testing:
- *
+ * The following callback should be injected:
  *   #postMessage() -- analogous to DedicatedWorkerGlobalScope#postMessage()
  */
 class _LMLayer {
-  handleMessage: (this: _LMLayer, e: MessageEvent) => void;
+  /**
+   * The function that handles messages from the keyboard.
+   */
+  protected handleMessage: (this: _LMLayer, e: MessageEvent) => void;
+  /**
+   * When defined by registerModel(), you can call this function to instantiate
+   * a new model.
+   */
+  protected createModel?: ModelFactory;
+  /**
+   * A injectable postMessage() analogue.
+   */
+  protected postMessage: PostMessage;
 
   constructor(postMessage: PostMessage = (self as DedicatedWorkerGlobalScope).postMessage) {
+    this.postMessage = postMessage;
     this.handleMessage = this.onMessageWhenUninitialized.bind(self);
   }
 
+  install(worker: LMLayerWorkerGlobalScope): this {
+    log("Installing...")
+    worker.onmessage = this.onMessage.bind(this);
+    worker.registerModel = this.registerModel.bind(this);
+    return this;
+  }
+
+  /**
+   * Handles messages from the keyboard.
+   *
+   * Does some error checking, then delegates to onMessage().
+   */
   onMessage(event: MessageEvent) {
     const {message} = event.data;
     if (!message) {
       throw new Error(`Message did not have a 'message' attribute: ${event.data}`);
     }
+    log('onMessage....')
 
     /* Delegate to the current onMessage() handler. */
-    return onMessage(event);
+    return this.handleMessage(event);
   }
 
-  protected onMessageWhenUninitialized(event: MessageEvent): void {
-    onMessageWhenUninitialized(event);
-  }
-}
-
-/**
- * The function that handles messages from the keyboard.
- */
-let onMessage = onMessageWhenUninitialized;
-
-type ModelFactory = (c: RequestedConfiguration) => Model;
-
-/**
- * When defined by registerModel(), you can call this function to instantiate
- * a new model.
- */
-let createModel: ModelFactory | undefined;
-
-/**
- * Model definition files must call registerModel() once in order to register
- * a function that returns an initialized Model instance to the LMLayer. The
- * LMLayer may then use the registered Model instance to perform predictions.
- */
-(self as LMLayerWorkerGlobalScope).registerModel = function registerModel(modelFactory: ModelFactory) {
-  createModel = modelFactory;
-};
-
-let _lmLayer = new _LMLayer();
-
-/**
- * Handles messages from the keyboard.
- *
- * Does some error checking, then delegates to onMessage().
- */
-(self as DedicatedWorkerGlobalScope).onmessage = _lmLayer.onMessage.bind(_lmLayer);
-
-/**
- * Handles message when uninitialized.
- *
- * Responds only to the `initialize` message.
- */
-function onMessageWhenUninitialized(event: MessageEvent) {
-  const {message} = event.data as Message;
-
-  if (message !== 'initialize') {
-    throw new Error('invalid message');
-  }
-  const {configuration} = event.data;
-
-  // Import the model.
-  let model = loadModel(event.data.model, configuration);
-  transitionToReadyState(model);
-
-  // Ready! Send desired configuration.
-  cast('ready', { configuration: model.configuration });
-}
-
-/**
- * Call this to transition to the 'ready' state.
- *
- * Sets the onMessage handler to the ready handler, with a model.
- */
-function transitionToReadyState(model: Model) {
   /**
-   * Responds to `predict` messages with a `suggestions` message.
+   * Model definition files must call registerModel() once in order to register
+   * a function that returns an initialized Model instance to the LMLayer. The
+   * LMLayer may then use the registered Model instance to perform predictions.
    */
-  onMessage = function onMessageWhenReady(event) {
-    const {message, token, transform, context} = event.data;
+  registerModel(modelFactory: ModelFactory) {
+    this.createModel = modelFactory;
+  };
 
-    if (message !== 'predict') {
+  /**
+   * Handles message when uninitialized.
+   *
+   * Responds only to the `initialize` message.
+   */
+  protected onMessageWhenUninitialized(event: MessageEvent) {
+    const {message} = event.data as Message;
+
+    if (message !== 'initialize') {
       throw new Error('invalid message');
     }
+    const {configuration} = event.data;
 
-    // XXX: induce the other end to reject the promise, because of a
-    // token/message mismatch. This is for testing purposes.
-    if (token === null) {
-      // @ts-ignore
-      cast('invalid', {token});
-      return;
-    }
+    // Import the model.
+    let model = this.loadModel(event.data.model, configuration);
+    this.transitionToReadyState(model);
 
-    let rawSuggestions = model.predict(context, transform);
-
-    // Sort in-place according to weight, ascending.
-    rawSuggestions.sort((a, b) => a.weight - b.weight);
-
-    // Convert the internal suggestion format to the one required by the keyboard.
-    let suggestions: Suggestion[] = rawSuggestions.map((internal) => {
-      let displayAs = internal.displayAs;
-
-      // Try to make up a display string.
-      if (displayAs === null || displayAs === undefined) {
-        displayAs = internal.transform.insert;
-      }
-
-      return {
-        transform: internal.transform,
-        displayAs
-      };
-    });
-
-    cast('suggestions', { token, suggestions });
-  };
-}
-
-/**
- * Send a message to the keyboard.
- */
-function cast(message: MessageKind, parameters: {}) {
-  postMessage({message, ...parameters });
-}
-
-/**
- * Loads the model from a separate file.
- */
-function loadModel(path : string , configuration: RequestedConfiguration) {
-  importScripts(path);
-  /**
-   * The model MUST call registerModel() which ultimately defines
-   * createModel() to a function.
-   */
-  if (createModel === undefined) {
-    throw new Error('Did not register a model!');
+    // Ready! Send desired configuration.
+    this.cast('ready', { configuration: model.configuration });
   }
 
-  return createModel(configuration);
+  /**
+   * Loads the model from a separate file.
+   */
+  protected loadModel(path: string, configuration: RequestedConfiguration) {
+    log('Loading model from', path)
+    importScripts(path);
+    /**
+     * The model MUST call registerModel() which ultimately defines
+     * createModel() to a function.
+     */
+    if (this.createModel === undefined) {
+      throw new Error('Did not register a model!');
+    }
+
+    return this.createModel(configuration);
+  }
+
+  /**
+   * Call this to transition to the 'ready' state.
+   *
+   * Sets the onMessage handler to the ready handler, with a model.
+   */
+  protected transitionToReadyState(model: Model) {
+    /**
+     * Responds to `predict` messages with a `suggestions` message.
+     */
+    this.handleMessage = (event: MessageEvent) => {
+      const {message, token, transform, context} = event.data;
+
+      if (message !== 'predict') {
+        throw new Error('invalid message');
+      }
+
+      // XXX: induce the other end to reject the promise, because of a
+      // token/message mismatch. This is for testing purposes.
+      if (token === null) {
+        // @ts-ignore
+        this.cast('invalid', {token});
+        return;
+      }
+
+      let rawSuggestions = model.predict(context, transform);
+
+      // Sort in-place according to weight, ascending.
+      rawSuggestions.sort((a, b) => a.weight - b.weight);
+
+      // Convert the internal suggestion format to the one required by the keyboard.
+      let suggestions: Suggestion[] = rawSuggestions.map((internal) => {
+        let displayAs = internal.displayAs;
+
+        // Try to make up a display string.
+        if (displayAs === null || displayAs === undefined) {
+          displayAs = internal.transform.insert;
+        }
+
+        return {
+          transform: internal.transform,
+          displayAs
+        };
+      });
+
+      this.cast('suggestions', { token, suggestions });
+    };
+  }
+
+  /**
+   * Send a well-formatted message to the keyboard.
+   */
+  protected cast(message: MessageKind, parameters: {}) {
+    // Delegate to the (potentially dependency-injected) postMessage.
+    this.postMessage({message, ...parameters });
+  }
 }
 
-/**
- * HACK: on Node.JS + tiny-worker, ensure code imported with importScripts()
- * has access to the same things defined on self in this file.
- */
-if (typeof global !== 'undefined') {
-  let globalPrototype = Object.getPrototypeOf(global);
-  Object.setPrototypeOf(self, globalPrototype);
-  Object.setPrototypeOf(global, self);
+log('Instantiating...')
+/* Set up the default instance of the LMLayer. */
+if (typeof self !== 'undefined') {
+  /**
+   * HACK: on Node.JS + tiny-worker, ensure code imported with importScripts()
+   * has access to the same things defined on self in this file.
+   */
+  let _lmLayer = (new _LMLayer).install(self as LMLayerWorkerGlobalScope);
+  if (typeof global !== 'undefined') {
+    let globalPrototype = Object.getPrototypeOf(global);
+    Object.setPrototypeOf(self, globalPrototype);
+    Object.setPrototypeOf(global, self);
+  }
+} else {
+  log('Refusing to register on self.')
+  // @ts-ignore
+  this._LMLayer = _LMLayer;
+}
+
+
+
+function log(...messages: any[]) {
+  let error = new Error;
+  let indent = error.stack
+    ? ' '.repeat((error.stack.match(/\n/g) || []).length - 2)
+    : '??? ';
+  console.log(indent, ...messages);
 }
 
 /*global importScripts*/
